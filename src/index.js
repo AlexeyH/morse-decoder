ebaef307e5821cc12de943896ee46117dbc8f306
AlexeyH <mailkeeper@tut.by>
2019-10-13 16:09:16 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,31 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let input_num_string = expr;
+    let encoded_symbol_string = "";
+    let decoded_symbol_string = "";
+    let ten_symbols_counter = 0;
+    for (let counter = 0; counter <= input_num_string.length; counter +=2) {
+        let substring = input_num_string.substring(counter, counter + 2);
+        if (substring == "10")
+            encoded_symbol_string += ".";
+        else if (substring == "11")
+            encoded_symbol_string += "-";
+        else if (substring == "**") {
+            counter += 8;
+            ten_symbols_counter = 0; 
+            decoded_symbol_string += " ";
+            encoded_symbol_string = "";
+            continue; 
+        }
+        ten_symbols_counter += 2;
+        if (ten_symbols_counter == 10) {
+            ten_symbols_counter = 0; 
+            decoded_symbol_string += MORSE_TABLE[encoded_symbol_string];
+            encoded_symbol_string = "";
+        }   
+    }
+    return (decoded_symbol_string);
 }
 
 module.exports = {
